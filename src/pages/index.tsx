0a2460f8c1cfee73ce07f5e61cac828e401f1fcf
carlosdoria <carlosdoria953@gmail.com>
2021-09-12 21:51:14 -0300

    feat: create the integration with prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,43 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  console.log(postsPagination);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return <h1>oi</h1>;
+}
 
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
+
+  const posts = postsResponse.results.map(postResponse => {
+    return {
+      uid: postResponse.uid,
+      first_publication_date: format(
+        new Date(postResponse.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: postResponse.data.title[0].text,
+        subtitle: postResponse.data.subtitle[0].text,
+        author: postResponse.data.author[0].text,
+      },
+    };
+  });
+
+  // eslint-disable-next-line no-console
+  // console.log(posts);
+
+  return {
+    props: {
+      results: posts,
+      next_page: '',
+    },
+  };
+};
