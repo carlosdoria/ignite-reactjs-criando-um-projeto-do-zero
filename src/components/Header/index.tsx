fd7c8593cfa96935420f6161e29156e9ed9da121
carlosdoria <carlosdoria953@gmail.com>
2021-09-13 20:19:07 -0300

    feat: create component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
 export default function Header() {
-  // TODO
+  const route = useRouter();
+  return (
+    <header>
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width="100%"
+        height="105px"
+        onClick={() => route.push('/')}
+      />
+    </header>
+  );
 }
