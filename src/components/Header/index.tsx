b3c8dd98faa0493a7820243beff2df5dc60bef19
carlosdoria <carlosdoria953@gmail.com>
2021-09-14 20:08:29 -0300

    feat: finish component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
+import styles from './header.module.scss';
 
-export default function Header() {
-  const route = useRouter();
+export default function Header(): JSX.Element {
   return (
-    <header>
-      <Image
-        src="/logo.svg"
-        alt="logo"
-        width="100%"
-        height="105px"
-        onClick={() => route.push('/')}
-      />
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        {' '}
+        <Link href="/">
+          <a>
+            <Image src="/logo.svg" alt="logo" width={240} height={26} />
+          </a>
+        </Link>
+      </div>
     </header>
   );
 }
